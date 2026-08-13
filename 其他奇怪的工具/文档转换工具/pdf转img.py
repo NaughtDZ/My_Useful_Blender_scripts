48e9df60d1ab50c741ddc148c06294fbf2c87a1e
NaughtDZ <37768911+NaughtDZ@users.noreply.github.com>
2026-08-13 22:24:55 +0800

    Add files via upload

---
+++ 其他奇怪的工具/文档转换工具/pdf转img.py
@@ -0,0 +1,8 @@
+﻿import fitz
+
+pdf_path = "『甘々と甘出し』台本（製品版）.pdf"
+doc = fitz.open(pdf_path)
+
+for i, page in enumerate(doc):
+    pix = page.get_pixmap(dpi=150)   # 控制清晰度，默认72
+    pix.save(f"page_{i+1}.png")
